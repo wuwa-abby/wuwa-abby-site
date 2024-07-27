f8b064df34e3cbc215ad4aa0086df3cd0ad78fbe
Zeroo <77537889+Zeroo28@users.noreply.github.com>
2024-07-28 05:08:41 +0800

    fix: menubar items never have 'active' state

--- src/app/core/services/navbar.service.ts
+++ src/app/core/services/navbar.service.ts
@@ -3,56 +3,75 @@ import { Injectable } from '@angular/core';
 import { MenuItem } from 'primeng/api';
 
 @Injectable({
-  providedIn: 'root',
+	providedIn: 'root',
 })
 export class NavbarService {
-  constructor() {}
+	constructor() {}
 
-  public activeRoute?: string;
-  public navbarItems: MenuItem[] = [
-    {
-      label: 'Convenes',
-      icon: 'bi bi-house',
-      routerLink: 'convenes',
-      styleClass: this.activeRoute === 'convenes' ? 'fw-semibold' : '',
-    },
-    {
-      label: 'Resonators',
-      icon: 'pi pi-users',
-      routerLink: 'resonators',
-      styleClass: this.activeRoute === 'resonators' ? 'fw-semibold' : '',
-    },
-    {
-      label: 'Statistics',
-      icon: 'pi pi-chart-bar',
-      styleClass: this.activeRoute?.startsWith('stats/') ? 'fw-semibold' : '',
-      items: [
-        {
-          label: 'My stats',
-          icon: 'pi pi-user',
-          routerLink: 'stats/personal',
-          styleClass:
-            this.activeRoute === 'stats/personal' ? 'fw-semibold' : '',
-        },
-        {
-          label: 'Global stats',
-          icon: 'pi pi-globe',
-          routerLink: 'stats/global',
-          styleClass: this.activeRoute === 'stats/global' ? 'fw-semibold' : '',
-        },
-        {
-          label: 'Share',
-          icon: 'pi pi-share-alt',
-          routerLink: 'stats/share',
-          styleClass: this.activeRoute === 'stats/share' ? 'fw-semibold' : '',
-        },
-      ],
-    },
-    {
-      label: 'Settings',
-      icon: 'bi bi-gear',
-      routerLink: 'settings',
-      styleClass: this.activeRoute === 'settings' ? 'fw-semibold' : '',
-    },
-  ];
+	public activeRoute?: string;
+	public navbarItems: MenuItem[] = [
+		{
+			label: 'Convenes',
+			icon: 'bi bi-house',
+			routerLink: 'convenes',
+			styleClass:
+				this.activeRoute === '/convenes'
+					? 'fw-semibold text-decoration-underline'
+					: '',
+		},
+		{
+			label: 'Resonators',
+			icon: 'pi pi-users',
+			routerLink: 'resonators',
+			styleClass:
+				this.activeRoute === '/resonators'
+					? 'fw-semibold text-decoration-underline'
+					: '',
+		},
+		{
+			label: 'Statistics',
+			icon: 'pi pi-chart-bar',
+			styleClass: this.activeRoute?.startsWith('/stats/')
+				? 'fw-semibold text-decoration-underline'
+				: '',
+			items: [
+				{
+					label: 'My stats',
+					icon: 'pi pi-user',
+					routerLink: 'stats/personal',
+					styleClass:
+						this.activeRoute === '/stats/personal'
+							? 'fw-semibold text-decoration-underline'
+							: '',
+				},
+				{
+					label: 'Global stats',
+					icon: 'pi pi-globe',
+					routerLink: 'stats/global',
+					styleClass:
+						this.activeRoute === '/stats/global'
+							? 'fw-semibold text-decoration-underline'
+							: '',
+				},
+				{
+					label: 'Share',
+					icon: 'pi pi-share-alt',
+					routerLink: 'stats/share',
+					styleClass:
+						this.activeRoute === '/stats/share'
+							? 'fw-semibold text-decoration-underline'
+							: '',
+				},
+			],
+		},
+		{
+			label: 'Settings',
+			icon: 'bi bi-gear',
+			routerLink: 'settings',
+			styleClass:
+				this.activeRoute === '/settings'
+					? 'fw-semibold text-decoration-underline'
+					: '',
+		},
+	];
 }
