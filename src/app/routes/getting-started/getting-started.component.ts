41ff0f4dd0037ffa430ef6e29ded4dc5324b3fc8
Zeroo <77537889+Zeroo28@users.noreply.github.com>
2024-07-28 07:00:33 +0800

    feat: WIP: Getting started page

--- src/app/routes/getting-started/getting-started.component.ts
+++ src/app/routes/getting-started/getting-started.component.ts
@@ -3,17 +3,18 @@ import { RouterModule } from '@angular/router';
 
 import { ButtonModule } from 'primeng/button';
 import { SkeletonModule } from 'primeng/skeleton';
+import { TooltipModule } from 'primeng/tooltip';
 
 @Component({
 	selector: 'abby-getting-started',
 	standalone: true,
-	imports: [RouterModule, ButtonModule, SkeletonModule],
+	imports: [RouterModule, ButtonModule, SkeletonModule, TooltipModule],
 	templateUrl: './getting-started.component.html',
 	styleUrl: './getting-started.component.scss',
 })
 export class GettingStartedComponent {
-	public scrollToMore() {
-		const elem = document.getElementById('track');
+	public scrollTo(elemId: string) {
+		const elem = document.getElementById(elemId);
 		if (elem) {
 			elem.scrollIntoView({ behavior: 'smooth', block: 'center' });
 		}
