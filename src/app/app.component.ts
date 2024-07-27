3d2713cf8bcf864d02814878c2e381f13a514efe
Zeroo <77537889+Zeroo28@users.noreply.github.com>
2024-07-28 01:47:25 +0800

    feat: WIP: Add footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { NavbarComponent } from '@core/components/navbar/navbar.component';
+import { FooterComponent } from '@core/components/footer/footer.component';
 
 @Component({
   selector: 'abby-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
