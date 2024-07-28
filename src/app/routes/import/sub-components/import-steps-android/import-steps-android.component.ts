df0ce81a1d5ef262e81fac08cdb4b3e38042b60e
Zeroo <77537889+Zeroo28@users.noreply.github.com>
2024-07-29 06:42:20 +0800

    feat: WIP: android import

--- src/app/routes/import/sub-components/import-steps-android/import-steps-android.component.ts
+++ src/app/routes/import/sub-components/import-steps-android/import-steps-android.component.ts
@@ -1,9 +1,22 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 
+import { FieldsetModule } from 'primeng/fieldset';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { InputTextModule } from 'primeng/inputtext';
+import { PanelModule } from 'primeng/panel';
 @Component({
 	selector: 'abby-import-steps-android',
 	standalone: true,
-	imports: [],
+	imports: [
+		FieldsetModule,
+		NgOptimizedImage,
+		IconFieldModule,
+		InputIconModule,
+		InputTextModule,
+		PanelModule,
+	],
 	templateUrl: './import-steps-android.component.html',
 	styleUrl: './import-steps-android.component.scss',
 })
