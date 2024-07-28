7cee992e5d1ced9ff15b7df31df4e80862028a5a
Zeroo <77537889+Zeroo28@users.noreply.github.com>
2024-07-28 19:55:21 +0800

    chore: Remove console log, tasks.json and meta-icon backup file

--- src/app/core/services/preferences.service.ts
+++ src/app/core/services/preferences.service.ts
@@ -4,46 +4,45 @@ import { Router } from '@angular/router';
 import { CookieService } from './cookie.service';
 
 @Injectable({
-  providedIn: 'root',
+	providedIn: 'root',
 })
 export class PreferencesService {
-  constructor(private router: Router, private cookieService: CookieService) {
-    const cookie = this.cookieService.get('userPreferences');
+	constructor(private router: Router, private cookieService: CookieService) {
+		const cookie = this.cookieService.get('userPreferences');
 
-    if (cookie) this.userPreferences = JSON.parse(cookie);
-    else this.setDefaultPreferences();
-  }
+		if (cookie) this.userPreferences = JSON.parse(cookie);
+		else this.setDefaultPreferences();
+	}
 
-  private userPreferences: Record<string, any> = {};
+	private userPreferences: Record<string, any> = {};
 
-  private readonly defaultOnHomeClick: string = '/convenes';
+	private readonly defaultOnHomeClick: string = '/convenes';
 
-  public onHomeClick(evt?: Event): void {
-    console.debug('PreferencesService: onHomeClick', evt);
-    if (evt) {
-      evt.preventDefault();
-    }
+	public onHomeClick(evt?: Event): void {
+		if (evt) {
+			evt.preventDefault();
+		}
 
-    this.router.navigate([
-      this.userPreferences['onHomeClick'] ?? this.defaultOnHomeClick,
-    ]);
-  }
+		this.router.navigate([
+			this.userPreferences['onHomeClick'] ?? this.defaultOnHomeClick,
+		]);
+	}
 
-  public remove(key: string): void {
-    delete this.userPreferences[key];
-    this.cookieService.set(
-      'userPreferences',
-      JSON.stringify(this.userPreferences)
-    );
-  }
+	public remove(key: string): void {
+		delete this.userPreferences[key];
+		this.cookieService.set(
+			'userPreferences',
+			JSON.stringify(this.userPreferences)
+		);
+	}
 
-  private setDefaultPreferences(): void {
-    this.userPreferences = {
-      onHomeClick: this.defaultOnHomeClick,
-    };
-    this.cookieService.set(
-      'userPreferences',
-      JSON.stringify(this.userPreferences)
-    );
-  }
+	private setDefaultPreferences(): void {
+		this.userPreferences = {
+			onHomeClick: this.defaultOnHomeClick,
+		};
+		this.cookieService.set(
+			'userPreferences',
+			JSON.stringify(this.userPreferences)
+		);
+	}
 }
