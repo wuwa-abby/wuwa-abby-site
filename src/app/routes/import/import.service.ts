951c74039ba0e0e43e4cfb8bbd86e7a3b19826a6
Zeroo <77537889+Zeroo28@users.noreply.github.com>
2024-07-29 08:11:52 +0800

    feat: Import android

--- src/app/routes/import/import.service.ts
+++ src/app/routes/import/import.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Injectable({
 	providedIn: 'root',
@@ -9,6 +9,20 @@ export class ImportService {
 
 	public importForm = new FormGroup({
 		platform: new FormControl<string>('and'),
-		historyUrl: new FormControl<string | undefined>(undefined),
+		historyUrl: new FormControl<string | undefined>(undefined, [
+			Validators.required,
+			Validators.pattern(
+				/https:\/\/aki-gm-resources-oversea\.aki-game\.net\/aki\/gacha\/index\.html#\/record\?.*/
+			),
+		]),
 	});
+
+	public getInvalidUrlCSSClass() {
+		const control = this.importForm.get('historyUrl');
+		if (control?.pristine || control?.valid) {
+			return undefined;
+		}
+
+		return 'ng-dirty ng-invalid';
+	}
 }
--- src/app/routes/import/sub-components/import-steps-android/import-steps-android.component.ts
+++ src/app/routes/import/sub-components/import-steps-android/import-steps-android.component.ts
@@ -1,10 +1,11 @@
 import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
-import { FieldsetModule } from 'primeng/fieldset';
 import { PanelModule } from 'primeng/panel';
 import { InputTextareaModule } from 'primeng/inputtextarea';
+import { MessagesModule } from 'primeng/messages';
+import { Message } from 'primeng/api';
 
 import { ImportService } from '../../import.service';
 
@@ -12,11 +13,11 @@ import { ImportService } from '../../import.service';
 	selector: 'abby-import-steps-android',
 	standalone: true,
 	imports: [
-		FieldsetModule,
 		NgOptimizedImage,
 		PanelModule,
 		InputTextareaModule,
 		ReactiveFormsModule,
+		MessagesModule,
 	],
 	templateUrl: './import-steps-android.component.html',
 	styleUrl: './import-steps-android.component.scss',
@@ -24,7 +25,48 @@ import { ImportService } from '../../import.service';
 export class ImportStepsAndroidComponent {
 	constructor(private service: ImportService) {}
 
+	public message: Message[] = [];
+
+	private changeTimeout?: any;
+
 	public get form() {
 		return this.service.importForm;
+	}
+
+	public getInvalidClass = this.service.getInvalidUrlCSSClass.bind(
+		this.service
+	);
+
+	public onChange() {
+		if (this.changeTimeout) clearTimeout(this.changeTimeout);
+
+		this.changeTimeout = setTimeout(() => {
+			const control = this.form.get('historyUrl');
+
+			this.message = [];
+			if (control?.errors?.['required']) {
+				this.message.push({
+					severity: 'error',
+					summary: 'Eh?',
+					detail: "Wubby can't help you if you don't give me a URL!",
+					closable: false,
+				});
+			} else if (control?.errors?.['pattern']) {
+				this.message.push({
+					severity: 'error',
+					summary: 'Huh?',
+					detail: "Wubby can't understand that URL. Try again?",
+					closable: false,
+				});
+			} else if (control?.valid) {
+				this.message.push({
+					severity: 'success',
+					summary: 'Nice!',
+					detail:
+						"Let's see what we can do with that URL! Click on the 'Next' button to continue.",
+					closable: true,
+				});
+			}
+		}, 250);
 	}
 }
