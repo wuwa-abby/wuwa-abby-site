f1cce1678d4b6e797e726229d9049ff67c2148b4
Zeroo <77537889+Zeroo28@users.noreply.github.com>
2024-07-30 12:03:12 +0800

    feat: Add android-bash script

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,15 +4,25 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideClientHydration } from '@angular/platform-browser';
 
 import { MessageService } from 'primeng/api';
+import { provideHighlightOptions } from 'ngx-highlightjs';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
-    provideClientHydration(),
-    MessageService,
-    provideAnimations(),
-  ],
+	providers: [
+		provideZoneChangeDetection({ eventCoalescing: true }),
+		provideRouter(routes),
+		provideClientHydration(),
+		MessageService,
+		provideAnimations(),
+		provideHighlightOptions({
+			coreLibraryLoader: () => import('highlight.js/lib/core'),
+			lineNumbersLoader: () => import('ngx-highlightjs/line-numbers'),
+			languages: {
+				typescript: () => import('highlight.js/lib/languages/json'),
+				css: () => import('highlight.js/lib/languages/bash'),
+				xml: () => import('highlight.js/lib/languages/powershell'),
+			},
+		}),
+	],
 };
--- src/app/routes/import/sub-components/import-steps-android/import-steps-android.component.ts
+++ src/app/routes/import/sub-components/import-steps-android/import-steps-android.component.ts
@@ -2,14 +2,45 @@ import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 
 import { PanelModule } from 'primeng/panel';
+import { ButtonModule } from 'primeng/button';
+import { InplaceModule } from 'primeng/inplace';
+import { MessagesModule } from 'primeng/messages';
+import { Message, MessageService } from 'primeng/api';
+import { Highlight } from 'ngx-highlightjs';
 
 @Component({
 	selector: 'abby-import-steps-android',
 	standalone: true,
-	imports: [NgOptimizedImage, PanelModule],
+	imports: [
+		NgOptimizedImage,
+		PanelModule,
+		ButtonModule,
+		Highlight,
+		InplaceModule,
+		MessagesModule,
+	],
 	templateUrl: './import-steps-android.component.html',
 	styleUrl: './import-steps-android.component.scss',
 })
 export class ImportStepsAndroidComponent {
-	constructor() {}
+	constructor(private toastService: MessageService) {}
+
+	public readonly bashScript = `SCRIPT_URL="https://wuwaabby.moe/redirect/linux-android-sh"; LOCAL_SCRIPT_PATH="/tmp/history-linux-android.sh"; curl -L -o $LOCAL_SCRIPT_PATH $SCRIPT_URL; chmod +x $LOCAL_SCRIPT_PATH; bash $LOCAL_SCRIPT_PATH"; rm -f $LOCAL_SCRIPT_PATH`;
+	public readonly warningMessage: Message[] = [
+		{
+			severity: 'warn',
+			summary: 'Heads up!',
+			detail:
+				'This command will download and execute a script from the internet. Wubby strongly recommends you to review any command/script before running it.',
+		},
+	];
+
+	public copyCommand() {
+		navigator.clipboard.writeText(this.bashScript);
+		this.toastService.add({
+			severity: 'success',
+			summary: 'Copied',
+			detail: 'The command has been copied to your clipboard.',
+		});
+	}
 }
