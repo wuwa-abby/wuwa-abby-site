844e9e2a2bbfe2ba1fc29fd857155a6c2bb07f75
Zeroo <77537889+Zeroo28@users.noreply.github.com>
2024-07-30 10:53:49 +0800

    fix: Move import URL UI to the right step

--- src/app/routes/import/import.component.ts
+++ src/app/routes/import/import.component.ts
@@ -1,11 +1,15 @@
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 import { FieldsetModule } from 'primeng/fieldset';
 import { SelectButtonModule } from 'primeng/selectbutton';
 import { StepperModule } from 'primeng/stepper';
 import { ButtonModule } from 'primeng/button';
+import { PanelModule } from 'primeng/panel';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { MessagesModule } from 'primeng/messages';
+import { Message } from 'primeng/api';
 
 import { PreferencesService } from '@app/core/services/preferences.service';
 
@@ -25,6 +29,9 @@ import { ImportService } from './import.service';
 		SelectButtonModule,
 		StepperModule,
 		ButtonModule,
+		PanelModule,
+		InputTextareaModule,
+		MessagesModule,
 
 		ImportStepsAndroidComponent,
 		ImportStepsWindowsComponent,
@@ -49,5 +56,46 @@ export class ImportComponent {
 
 	public get importForm(): FormGroup {
 		return this.service.importForm;
+	}
+
+	public message: Message[] = [];
+
+	private changeTimeout?: any;
+
+	public getInvalidClass = this.service.getInvalidUrlCSSClass.bind(
+		this.service
+	);
+
+	public onChange() {
+		if (this.changeTimeout) clearTimeout(this.changeTimeout);
+
+		this.changeTimeout = setTimeout(() => {
+			const control = this.importForm.get('historyUrl');
+
+			this.message = [];
+			if (control?.errors?.['required']) {
+				this.message.push({
+					severity: 'error',
+					summary: 'Eh?',
+					detail: "Wubby can't help you if you don't give me a URL!",
+					closable: false,
+				});
+			} else if (control?.errors?.['pattern']) {
+				this.message.push({
+					severity: 'error',
+					summary: 'Huh?',
+					detail: "Wubby can't understand that URL. Try again?",
+					closable: false,
+				});
+			} else if (control?.valid) {
+				this.message.push({
+					severity: 'success',
+					summary: 'Nice!',
+					detail:
+						"Let's see what we can do with that URL! Click on the 'Next' button to continue.",
+					closable: true,
+				});
+			}
+		}, 250);
 	}
 }
