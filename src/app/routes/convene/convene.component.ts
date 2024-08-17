6ca5fd983396e9c8a2e1d7ba2a121c522c11b8d6
MichMich <77537889+Zeroo28@users.noreply.github.com>
2024-08-17 16:59:00 +0800

    feat: WIP: add change url panel

--- src/app/routes/convene/convene.component.ts
+++ src/app/routes/convene/convene.component.ts
@@ -3,12 +3,40 @@ import { Component } from '@angular/core';
 
 import { ButtonModule } from 'primeng/button';
 import { FieldsetModule } from 'primeng/fieldset';
+import { PanelModule } from 'primeng/panel';
+import { MessagesModule } from 'primeng/messages';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { InputTextModule } from 'primeng/inputtext';
+import { Message } from 'primeng/api';
 
 @Component({
 	selector: 'abby-convene',
 	standalone: true,
-	imports: [CommonModule, ButtonModule, FieldsetModule],
+	imports: [
+		CommonModule,
+		ButtonModule,
+		FieldsetModule,
+		PanelModule,
+		MessagesModule,
+		IconFieldModule,
+		InputIconModule,
+		InputTextModule,
+	],
 	templateUrl: './convene.component.html',
 	styleUrl: './convene.component.scss',
 })
-export class ConveneComponent {}
+export class ConveneComponent {
+	constructor() {}
+
+	public readonly version12Message: Message[] = [
+		{
+			key: 'version1.2',
+			severity: 'warn',
+			closable: false,
+			summary: 'API change',
+			detail:
+				'With the release of version 1.2, Kuro games has removed the support for permanent convene URLs. This means Wubby will no longer be able to remember your convene URL.',
+		},
+	];
+}
