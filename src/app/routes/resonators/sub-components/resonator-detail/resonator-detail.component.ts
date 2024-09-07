fc2117a7dd5872251f2f4e4483eb28a6e1ddc413
MichMich <77537889+Zeroo28@users.noreply.github.com>
2024-09-07 11:19:26 +0800

    feat: Add breadcrumbs

--- src/app/routes/resonators/sub-components/resonator-detail/resonator-detail.component.ts
+++ src/app/routes/resonators/sub-components/resonator-detail/resonator-detail.component.ts
@@ -3,22 +3,36 @@ import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { ActivatedRoute, RouterModule } from '@angular/router';
 
 import { ButtonModule } from 'primeng/button';
+import { BreadcrumbModule } from 'primeng/breadcrumb';
+import { MenuItem } from 'primeng/api';
 
 import { ErrorOr } from '@core/types/error-or.type';
+import { NavbarService } from '@core/services/navbar.service';
 import { DisplayResonator } from '@routes/resonators/resonators.component';
 
 @Component({
 	selector: 'abby-resonator-detail',
 	standalone: true,
-	imports: [CommonModule, NgOptimizedImage, RouterModule, ButtonModule],
+	imports: [
+		CommonModule,
+		NgOptimizedImage,
+		RouterModule,
+		ButtonModule,
+		BreadcrumbModule,
+	],
 	templateUrl: './resonator-detail.component.html',
 	styleUrl: './resonator-detail.component.scss',
 })
 export class ResonatorDetailComponent implements OnInit {
 	constructor(
 		@Inject(PLATFORM_ID) private platformId: Object,
-		private activatedRoute: ActivatedRoute
-	) {}
+		private activatedRoute: ActivatedRoute,
+		public navbarService: NavbarService
+	) {
+		this.createBreadcrumbs();
+	}
+
+	public breadcrumbs!: MenuItem[];
 
 	private _resonator?: ErrorOr<DisplayResonator>;
 
@@ -36,5 +50,18 @@ export class ResonatorDetailComponent implements OnInit {
 
 	ngOnInit(): void {
 		this._resonator = this.activatedRoute.snapshot.data['resonator'];
+	}
+
+	private createBreadcrumbs(): void {
+		this.breadcrumbs = [
+			{
+				label: 'Resonators',
+				routerLink: '/resonators',
+			},
+			{
+				label: this.resonator?.name,
+				icon: this.resonator ? '' : 'pi pi-question',
+			},
+		];
 	}
 }
