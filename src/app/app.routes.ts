079c58603f0d4cbaddd4991ddf9c1eb52973cc94
MichMich <77537889+Zeroo28@users.noreply.github.com>
2024-09-15 22:15:20 +0800

    feat: WIP: add contribute home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
+
 import { ConveneComponent } from '@routes/convene/convene.component';
 import { conveneResolver } from '@routes/convene/convene.resolver';
 
+import { ContributeComponent } from '@routes/contribute/contribute.component';
 import { GettingStartedComponent } from '@routes/getting-started/getting-started.component';
 import { ImportComponent } from '@routes/import/import.component';
 import { NotFoundComponent } from '@routes/not-found/not-found.component';
@@ -28,6 +30,13 @@ export const routes: Routes = [
 		path: 'convenes',
 		component: ConveneComponent,
 		resolve: { banners: conveneResolver },
+	},
+
+	/* Contributors */
+	{
+		title: 'Contribute',
+		path: 'contribute',
+		component: ContributeComponent,
 	},
 
 	/* 404 redirect */
