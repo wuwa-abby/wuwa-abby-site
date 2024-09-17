cd521446670898b8858ba99ae3df78001ef8d020
MichMich <77537889+Zeroo28@users.noreply.github.com>
2024-09-17 08:06:08 +0800

    feat: WIP: Add maintain/create weapon and resonator components

---
+++ src/app/routes/contribute/sub-components/create-weapon/create-weapon.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'abby-create-weapon',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-weapon.component.html',
+  styleUrl: './create-weapon.component.scss'
+})
+export class CreateWeaponComponent {
+
+}
